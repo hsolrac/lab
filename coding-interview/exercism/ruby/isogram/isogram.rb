c1edf9b9100a72095e0d3e250ff3e11dacd688d7
hsolrac <ch.henriquevinha@gmail.com>
2024-03-18 15:53:05 -0300

    exercism: isogram

--- coding-interview/exercism/ruby/isogram/isogram.rb
+++ coding-interview/exercism/ruby/isogram/isogram.rb
@@ -2,20 +2,6 @@ class Isogram
   def self.isogram?(input)
     return true if input.empty?
     input.downcase!
-    input_reject_hifen = input.chars.reject { |w| w == '-' }
-    input_reject_hifen.uniq == input
-  end
-end
-
-class Array
-  def uniq(*args)
-    result = []
-    self.each do |element|
-     result << element unless result.include?(element)
-    end
-    return result if args.empty?
-    args.each { |ele| result << ele }
-
-    result
+    input.chars.uniq.delete_if { |c| c == '-' || c == ' ' } == input.chars.delete_if { |c| c == '-' || c == ' ' }
   end
 end
